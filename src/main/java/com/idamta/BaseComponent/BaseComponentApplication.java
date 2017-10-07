ca002e624d4070c8f086c02747e094f942f492df
gnnaveen <Naveen.kumar@idamta.com>
2017-10-07 16:38:50 +0530

    intial commit

---
+++ src/main/java/com/idamta/BaseComponent/BaseComponentApplication.java
@@ -0,0 +1,12 @@
+package com.idamta.BaseComponent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.idamta.*")
+public class BaseComponentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseComponentApplication.class, args);
+	}
+}
